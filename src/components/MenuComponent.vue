6cfa842aa348ec83633d86339b990e267d0e8a84
nurhiman_a <nurhimanangga@gmail.com>
2022-10-18 16:54:08 +0700

    :construction: feat(Menu): Component

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,4 +1,4 @@
-<template>  
+<template>
 	<div
 		class="container fixed-top py-3 d-flex flex-row justify-content-between align-items-center"
 		style="z-index: 1; background-color: transparent; padding: 0 60px"
@@ -11,24 +11,57 @@
 			<img
 				class="pointer"
 				src="@/assets/images/icons/menu.svg"
+				@click="showMenu = true"
 				v-if="theme == 'dark'"
 			/>
-			<img class="pointer" src="@/assets/images/icons/menu-dark.svg" v-else />
+			<img
+				class="pointer"
+				src="@/assets/images/icons/menu-dark.svg"
+				@click="showMenu = true"
+				v-else
+			/>
 		</transition>
 	</div>
+	<transition name="ease" mode="out-in">
+		<div
+			class="container-fluid"
+			style="
+				position: absolute;
+				height: 100vh;
+				width: 100vw;
+				background: rgba(0, 0, 0, 0.4);
+				backdrop-filter: blur(10px);
+				z-index: 2;
+			"
+			v-if="showMenu"
+		>
+			<div
+				class="container py-3 d-flex flex-row justify-content-end align-items-center"
+				style="padding: 0 60px"
+			>
+				<img
+					class="my-4 pointer"
+					src="@/assets/images/icons/close.svg"
+					@click="showMenu = false"
+				/>
+			</div>
+		</div>
+	</transition>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+	import { defineProps, ref } from 'vue';
 
-const props = defineProps({
-  theme: {
-    type: String,
-    require: true
-  },
-  direction: {
-    type: String,
-    require: true
-  },
-})
+	const props = defineProps({
+		theme: {
+			type: String,
+			require: true,
+		},
+		direction: {
+			type: String,
+			require: true,
+		},
+	});
+
+	const showMenu = ref(false);
 </script>
