4938867be7559e8b2ac5c9a85b73f9240230194e
nurhiman_a <nurhimanangga@gmail.com>
2022-10-19 16:01:56 +0700

    Route transition

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,54 @@
 <template>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Albert+Sans&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <div class="container-fluid scrollbar-custom">
-    <router-view></router-view>
-  </div>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="" />
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Albert+Sans&display=swap"
+			rel="stylesheet"
+		/>
+	</head>
+	<router-view v-slot="{ Component, route }">
+		<transition name="route" mode="out-in">
+			<div class="container-fluid" :key="route.name">
+				<component :is="Component"></component>
+			</div>
+		</transition>
+	</router-view>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
-// import Navbar from "./components/Navbar.vue";
+	import { RouterView } from 'vue-router';
 </script>
 
-
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Albert+Sans:wght@100;200;300;400;500;600;700&family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&family=Montserrat:wght@100;200;300;400;500;600;700;800&family=Open+Sans:wght@300;400;500;600;700&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Albert+Sans:wght@100;200;300;400;500;600;700&family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&family=Montserrat:wght@100;200;300;400;500;600;700;800&family=Open+Sans:wght@300;400;500;600;700&display=swap');
 
-#app {
-  font-family: 'Albert Sans', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #00000;
-}
+	#app {
+		font-family: 'Albert Sans', sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #00000;
+	}
 
-/* section {
-  height: 100vh;
-  display: flex;
-  color: white;
-  scroll-snap-align: start;
-  scroll-behavior: smooth;
-} */
+	.container-fluid {
+		height: 100vh;
+		padding: 0 !important;
+	}
 
-.container-fluid {
-  /* scroll-snap-type: y mandatory;
-  overflow-y: scroll; */
-  height: 100vh;
-  padding: 0 !important;
-  
-}
-
-/* @media  (max-width:481px){
-  .container-fluid{
-width: 100%;
-height: 50vh;
-  }
-  } */
-
-// scrollbar
-.scrollbar-custom::-webkit-scrollbar {
-    width: 5px;
-    background-color: #f5f5f5;
-}
-
-.scrollbar-custom::-webkit-scrollbar-thumb {
-    border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px #1e5fb6;
-    background-color: #1e5fb6;
-}
-
-.scrollbar-custom .v-data-table__wrapper::-webkit-scrollbar {
-    width: 5px;
-    height: 8px;
-    background-color: #f5f5f5;
-}
-
-.scrollbar-custom .v-data-table__wrapper::-webkit-scrollbar-thumb {
-    border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px #1e5fb6;
-    background-color: #1e5fb6;
-}
+	/* Route transitions */
+	.route-enter-from {
+		opacity: 0;
+	}
+	.route-enter-active {
+		transition: all 0.2s ease-out;
+	}
+	.route-leave-to {
+		opacity: 0;
+	}
+	.route-leave-active {
+		transition: all 0.2s ease-in;
+	}
 </style>
