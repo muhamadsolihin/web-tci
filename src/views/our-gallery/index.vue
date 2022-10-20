c4c2ef21a16675ec8566626d0e9f08715c67cc9a
nurhiman_a <nurhimanangga@gmail.com>
2022-10-20 13:53:03 +0700

    :hammer: Keep function for scroll

--- src/views/our-gallery/index.vue
+++ src/views/our-gallery/index.vue
@@ -1,14 +1,21 @@
 <template>
 	<MenuComponent :theme="'dark'" :direction="direction" />
-	<HeroSection />
+	<div id="our-gallery">
+		<HeroSection />
+		<GallerySection />
+	</div>
 </template>
 
 <script setup>
 	import MenuComponent from '@/components/MenuComponent.vue';
 	import HeroSection from './components/HeroSection.vue';
+	import GallerySection from './components/GallerySection.vue';
 </script>
 
 <style lang="scss" scoped>
+	#our-gallery {
+		overflow-y: scroll !important;
+	}
 	section {
 		min-height: 100vh;
 		display: flex;
