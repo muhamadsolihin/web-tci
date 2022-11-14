f15b9d9498adf8f1ca628701d69a951f9433c8d1
nurhiman_a <nurhimanangga@gmail.com>
2022-11-14 08:57:06 +0700

    Change navbar theme on our work and blog pages

--- src/views/Blog/index.vue
+++ src/views/Blog/index.vue
@@ -1,30 +1,26 @@
 <template>
-	<MenuComponent :theme="'light'" :direction="'dark'" @goMenu="scrollToTop" />
-	<div id="our-blog">
-		<HeroSection />
-		<Ourblog />
-		<FooterComponent
-			:sections-length="4"
-			:current-section="3"
-      @toTop="scrollToTop"
-		/>
-	</div>
+  <MenuComponent :theme="'dark'" :direction="'down'" @goMenu="scrollToTop" />
+  <div id="our-blog">
+    <HeroSection />
+    <Ourblog />
+    <FooterComponent :sections-length="4" :current-section="3" @toTop="scrollToTop" />
+  </div>
 </template>
 
 <script setup>
-	import MenuComponent from '@/components/MenuComponent.vue';
-	import HeroSection from './components/Home.vue';
-	import Ourblog from './components/content.vue';
-  import FooterComponent from "@/components/FooterComponent.vue";
+import MenuComponent from "@/components/MenuComponent.vue";
+import HeroSection from "./components/Home.vue";
+import Ourblog from "./components/content.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
-  const scrollToTop = () => {
-    window.scrollTo(0, 0)
-  }
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
 </script>
 
 <style lang="scss" scoped>
-	section {
-		min-height: 100vh;
-		display: flex;
-	}
+section {
+  min-height: 100vh;
+  display: flex;
+}
 </style>
--- src/views/our-work/index.vue
+++ src/views/our-work/index.vue
@@ -1,32 +1,28 @@
 <template>
-	<MenuComponent :theme="'light'" :direction="'dark'" @goMenu="scrollToTop" />
-	<div id="our-works">
-		<HeroSection />
-		<!-- <Ourwork /> -->
-		<Sample />
-		<FooterComponent
-			:sections-length="4"
-			:current-section="3"
-      @toTop="scrollToTop"
-		/>
-	</div>
+  <MenuComponent :theme="'dark'" :direction="'down'" @goMenu="scrollToTop" />
+  <div id="our-works">
+    <HeroSection />
+    <!-- <Ourwork /> -->
+    <Sample />
+    <FooterComponent :sections-length="4" :current-section="3" @toTop="scrollToTop" />
+  </div>
 </template>
 
 <script setup>
-	import MenuComponent from '@/components/MenuComponent.vue';
-	import HeroSection from './components/Home.vue';
-	import Ourwork from './components/ourwork.vue';
-	import Sample from './components/sample.vue';
-  import FooterComponent from "@/components/FooterComponent.vue";
+import MenuComponent from "@/components/MenuComponent.vue";
+import HeroSection from "./components/Home.vue";
+import Ourwork from "./components/ourwork.vue";
+import Sample from "./components/sample.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
-  const scrollToTop = () => {
-    window.scrollTo(0, 0)
-  }
+const scrollToTop = () => {
+  window.scrollTo(0, 0);
+};
 </script>
 
 <style lang="scss" scoped>
-	section {
-		min-height: 100vh;
-		display: flex;
-	}
+section {
+  min-height: 100vh;
+  display: flex;
+}
 </style>
